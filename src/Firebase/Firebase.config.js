0c07863bec8274f22c279969825a3b19539a1111
Muhammad Nurul Ahsan <86627956+Muhammadnurulahsan21@users.noreply.github.com>
2021-10-18 13:34:29 +0600

    new code

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5noJseBGWOyucBGDI-VBYJqQBihZBXas",
+  authDomain: "apollo-health-care.firebaseapp.com",
+  projectId: "apollo-health-care",
+  storageBucket: "apollo-health-care.appspot.com",
+  messagingSenderId: "162827514265",
+  appId: "1:162827514265:web:5f47e0b10508301deee308",
+  measurementId: "G-WNN4MP45GS",
+};
+export default firebaseConfig;
