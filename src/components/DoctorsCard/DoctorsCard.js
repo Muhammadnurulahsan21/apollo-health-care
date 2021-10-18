e5812fba63fc097ecbfb61178fbd1d2030ce8c89
Muhammad Nurul Ahsan <86627956+Muhammadnurulahsan21@users.noreply.github.com>
2021-10-18 19:11:32 +0600

    folder added

---
+++ src/components/DoctorsCard/DoctorsCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DoctorsCard.css'
+
+const DoctorsCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DoctorsCard;
---
+++ src/components/HomeServicesCard/HomeServicesCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeServicesCard.css'
+
+const HomeServicesCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeServicesCard;
